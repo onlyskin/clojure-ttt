d9f5785047c5918076841fe7400e2024ba6b0342
Sam Szreter <s.szreter@gmail.com>
2017-11-15 13:11:14 +0200

    Add board/winner(marker)

--- spec/clojure_ttt/board_spec.clj
+++ spec/clojure_ttt/board_spec.clj
@@ -3,24 +3,39 @@
             [clojure-ttt.board :refer :all]))
 
 (describe "make-board"
-  (it "has 9 empty strings"
-    (should= 
-      [" " " " " "
-       " " " " " "
-       " " " " " "]
-      (make-board))))
+          (it "has 9 empty strings"
+              (should= 
+                [" " " " " "
+                 " " " " " "
+                 " " " " " "]
+                (make-board))))
 
 (describe "play-on-board"
-  (it "plays on board"
-    (should=
-      [" " " " " "
-       "X" " " " "
-       " " " " " "]
-      (play-on-board (make-board) 4 "X"))))
+          (it "plays on board"
+              (should=
+                [" " " " " "
+                 "X" " " " "
+                 " " " " " "]
+                (play-on-board (make-board) 4 "X"))))
 
 (describe "available-moves"
           (it "gets available moves"
               (should=
                 [2, 3, 8, 9]
-                (available-moves
-                  ["X", " ", " ", "O", "O", "X", "X", " ", " "]))))
+                (available-moves ["X", " ", " ", "O", "O", "X", "X", " ", " "]))))
+
+(describe "winner"
+          (it "is true for X"
+              (should= true (winner ["X" "X" "X" "O" "O" " " " " " " " "] "X")))
+
+          (it "is false for X"
+              (should= false (winner ["X" "X" " " "O" "O" "O" "X" " " " "] "X")))
+          
+          (it "is true for O"
+              (should= true (winner ["X" "O" "X" " " "O" " " "X" "O" " "] "O")))
+          
+          (it "is true for X"
+              (should= true (winner ["X" "O" " " " " "X" "O" " " " " "X"] "X")))
+          
+          )
+
--- src/clojure_ttt/board.clj
+++ src/clojure_ttt/board.clj
@@ -10,5 +10,23 @@
 (defn available-moves [board]
   (map #(+ 1 (get % 0)) (filter #(empty-cell %) (map-indexed #(vec [%1, %2]) board))))
 
+(declare win-paths)
+(declare win-in-path)
+(defn winner [board marker]
+  (boolean (some #(win-in-path board marker %) (win-paths)))
+  )
+
 (defn- empty-cell [cell]
   (= " " (get cell 1)))
+
+(defn- win-paths [] [[0 1 2]
+                     [3 4 5]
+                     [6 7 8]
+                     [0 3 6]
+                     [1 4 7]
+                     [2 5 8]
+                     [0 4 8]
+                     [6 4 2]])
+
+(defn- win-in-path [board marker path]
+  (every? #(= % marker) (map #(get board %) path)))

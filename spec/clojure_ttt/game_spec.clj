d440c59f5956466bc38e6539bd08a37320f612eb
Sam Szreter <s.szreter@gmail.com>
2017-11-16 13:30:38 +0200

    Dry up game tests

--- spec/clojure_ttt/game_spec.clj
+++ spec/clojure_ttt/game_spec.clj
@@ -2,30 +2,29 @@
   (:require [speclj.core :refer :all]
             [clojure-ttt.game :refer :all])) 
 
+(defn out-str-from-run [moves]
+  (with-out-str
+    (with-in-str (clojure.string/join "\n" moves) (run-game))))
+
 (describe "run game"
           (it "output prints empty board"
               (should-contain
                 #"(?s)1.*2.*3.*4.*5.*6.*7.*8.*9"
-                (with-out-str
-                  (with-in-str "1\n4\n2\n5\n3" (run-game)))))
+                (out-str-from-run ["1" "4" "2" "5" "3"])))
 
           (it "output prints X"
               (should-contain
                 "X"
-                (with-out-str
-                  (with-in-str "1\n4\n2\n5\n3" (run-game)))))
+                (out-str-from-run ["1" "4" "2" "5" "3"])))
 
           (it "output prints O"
               (should-contain
                 "O"
-                (with-out-str
-                  (with-in-str "1\n4\n2\n5\n3" (run-game)))))
+                (out-str-from-run ["1" "4" "2" "5" "3"])))
 
           (it "ends" (should true))
 
           (it "output print X won"
               (should-contain
                 "X won"
-                (with-out-str
-                  (with-in-str "1\n4\n2\n5\n3" (run-game)))))
-          )
+                (out-str-from-run ["1" "4" "2" "5" "3"]))))

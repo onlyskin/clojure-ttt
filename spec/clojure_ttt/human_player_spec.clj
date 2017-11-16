bc5821712bf67c65ccd5ac2ba8a67eec411c9e1d
Sam Szreter <s.szreter@gmail.com>
2017-11-16 18:53:43 +0200

    Implement get-human-move

---
+++ spec/clojure_ttt/human_player_spec.clj
@@ -0,0 +1,12 @@
+(ns clojure-ttt.human-player-spec
+  (:require [speclj.core :refer :all]
+            [clojure-ttt.human-player :refer :all]))
+
+(defn- vec-for-string [board-string]
+  (clojure.string/split board-string #""))
+
+(describe "get-human-move"
+          (it "gets valid move from input"
+              (should= 3 (with-in-str "20\nst\n3\n"
+                (get-human-move (vec-for-string "         "))))))
+

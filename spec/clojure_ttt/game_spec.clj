d52c58b2b75783d2dfc5e40b444a11eaf8df991c
Sam Szreter <s.szreter@gmail.com>
2017-11-20 19:33:25 +0200

    TDD a human/computer game

--- spec/clojure_ttt/game_spec.clj
+++ spec/clojure_ttt/game_spec.clj
@@ -1,36 +1,56 @@
 (ns clojure-ttt.game-spec
   (:require [speclj.core :refer :all]
+            [clojure-ttt.board :refer :all]
             [clojure-ttt.game :refer :all]
+            [clojure-ttt.computer-player :refer :all]
             [clojure-ttt.human-player :refer :all])) 
 
-(defn out-str-from-run [moves]
-  (with-out-str
-    (with-in-str (clojure.string/join "\n" moves) (run-game [get-human-move get-human-move]))))
+(defn- get-stub-move [board]
+  (cond
+    (boolean (some #(= % 4) (available-moves board))) 4
+    :else 5))
 
-(describe "run game"
-          (it "output prints empty board"
+(defn out-str-from-h-h-game [moves]
+  (with-out-str
+    (with-in-str
+      (clojure.string/join "\n" moves)
+      (run-game [get-human-move get-human-move]))))
+
+(defn out-str-from-h-other-game [moves]
+  (with-out-str
+    (with-in-str
+      (clojure.string/join "\n" moves)
+      (run-game [get-human-move get-stub-move]))))
+
+(describe "run human human game"
+          (it "ends" (should true))
+
+          (it "output prints empty board and markers"
               (should-contain
                 #"(?s)1.*2.*3.*4.*5.*6.*7.*8.*9"
-                (out-str-from-run ["1" "4" "2" "5" "3"])))
-
-          (it "output prints X"
-              (should-contain
-                "X"
-                (out-str-from-run ["1" "4" "2" "5" "3"])))
-
-          (it "output prints O"
+                (out-str-from-h-h-game ["1" "4" "2" "5" "3"])) 
+              
               (should-contain
                 "O"
-                (out-str-from-run ["1" "4" "2" "5" "3"])))
+                (out-str-from-h-h-game ["1" "4" "2" "5" "3"]))
 
-          (it "ends" (should true))
+              (should-contain
+                "X"
+                (out-str-from-h-h-game ["1" "4" "2" "5" "3"]))
+              )
 
           (it "output prints X won"
               (should-contain
                 "X won"
-                (out-str-from-run ["1" "4" "2" "5" "3"])))
+                (out-str-from-h-h-game ["1" "4" "2" "5" "3"])))
 
           (it "rejects bad moves"
               (should-contain
                 "X won"
-                (out-str-from-run ["1" "4" "st" "2" "5" "20" "3"]))))
+                (out-str-from-h-h-game ["1" "4" "st" "2" "5" "20" "3"]))))
+
+(describe "run human other game"
+          (it "output prints X won"
+              (should-contain
+                "X won"
+                (out-str-from-h-other-game ["1" "2" "3"]))))
--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,5 +1,8 @@
 (ns clojure-ttt.core
-  (:require [clojure-ttt.game :refer :all]))
+  (:require [clojure-ttt.game :refer :all])
+  (:require [clojure-ttt.human-player :refer :all])
+  (:require [clojure-ttt.computer-player :refer :all])
+  )
 
 (defn -main [& args]
-  (run-game))
+  (run-game [get-human-move get-negamax-move]))

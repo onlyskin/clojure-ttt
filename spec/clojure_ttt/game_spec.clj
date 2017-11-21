cb0264ffed2bc372ccc2ea9ff26a994a0fdef1f1
Sam Szreter <s.szreter@gmail.com>
2017-11-21 17:55:42 +0200

    TDD playing different game types

--- spec/clojure_ttt/game_spec.clj
+++ spec/clojure_ttt/game_spec.clj
@@ -47,10 +47,55 @@
           (it "rejects bad moves"
               (should-contain
                 "X won"
-                (out-str-from-h-h-game ["1" "4" "st" "2" "5" "20" "3"]))))
+                (out-str-from-h-h-game
+                  ["1" "4" "st" "2" "5" "20" "3"]))))
 
 (describe "run human other game"
           (it "output prints X won"
               (should-contain
                 "X won"
                 (out-str-from-h-other-game ["1" "2" "3"]))))
+
+(describe "make game"
+          (with-stubs)
+
+          (it "prints options"
+              (with-redefs
+                [run-game (stub :run-game)]
+               (should-contain
+                 #"(?s)Player 1 type: \(h\)uman or \(c\)omputer"
+                 (with-out-str
+                   (with-in-str "h\nh\n" (make-game))))
+               (should-contain
+                 #"(?s)Player 2 type: \(h\)uman or \(c\)omputer"
+                 (with-out-str
+                   (with-in-str "h\nh\n" (make-game))))))
+
+          (it "calls run-game with human move funcs when input is human human"
+              (with-redefs
+                [run-game (stub :run-game)]
+                (with-in-str "h\nh\n" (make-game))
+
+                (should-have-invoked
+                  :run-game
+                  {:with [[get-human-move get-human-move]]})))
+
+          (it "calls run-game with human move and negamax move when input is human computer"
+              (with-redefs
+                [run-game (stub :run-game)]
+                (with-in-str "h\nc\n" (make-game))
+
+                (should-have-invoked
+                  :run-game
+                  {:with [[get-human-move get-negamax-move]]})))
+
+          (it "calls run-game with negamax move and human move when input is computer human"
+              (with-redefs
+                [run-game (stub :run-game)]
+                (with-in-str "c\nh\n" (make-game))
+
+                (should-have-invoked
+                  :run-game
+                  {:with [[get-negamax-move get-human-move]]})))
+
+          )
--- src/clojure_ttt/game.clj
+++ src/clojure_ttt/game.clj
@@ -1,5 +1,7 @@
 (ns clojure-ttt.game
   (:require [clojure-ttt.ui :refer :all])
+  (:require [clojure-ttt.human-player :refer :all])
+  (:require [clojure-ttt.computer-player :refer :all])
   (:require [clojure-ttt.board :refer :all]))
 
 (defn- get-move-function [move-functions, depth]
@@ -20,4 +22,14 @@
 
 (defn run-game [move-functions]
   (play-turn move-functions 0 (make-board)))
+
+(defn- move-func-from-choice [choice]
+  ({"h" get-human-move "c" get-negamax-move} choice))
+
+(defn make-game []
+  (output "Player 1 type: (h)uman or (c)omputer")
+  (output "Player 2 type: (h)uman or (c)omputer")
+  (run-game [(move-func-from-choice (input))
+             (move-func-from-choice (input))
+             ]))
 
